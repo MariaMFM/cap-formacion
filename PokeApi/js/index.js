a4ce71b831d5fb7c658b4929bb98f5b72b5be1a4
MariaMFM <115450137+MariaMFM@users.noreply.github.com>
2023-09-06 16:09:56 +0200

    Add pokeApi project

---
+++ PokeApi/js/index.js
@@ -0,0 +1,32 @@
+const pokemonList = document.querySelector("#pokemonList");
+let URL = "https://pokeapi.co/api/v2/pokemon/";
+
+for (let i = 1; i <= 151; i++) {
+   fetch(URL + i)
+    .then((Response) => Response.json())
+    .then(data => sawPokemon(data)) 
+}
+
+function sawPokemon(pokeData) {
+
+    let pokeId = pokeData.id.toString();
+    if (pokeId.length === 1) {
+        pokeId = "00" + pokeId;
+    } else if (pokeId.length === 2) {
+        pokeId = "0" + pokeId;
+    }
+
+    const div = document.createElement("div");
+    div.classList.add("pokemon");
+    div.innerHTML = `
+        
+        <div class="pokeImg">
+            <img src="${pokeData.sprites.other["official-artwork"].front_default}" alt="${pokeData.name}">
+        </div>
+         <div class="pokeInfo">
+            <p class="pokeId">#${pokeId}</p>
+            <h2 class="pokeName">${pokeData.name}</h2>
+        </div>
+    `;
+    pokemonList.append(div);
+}
